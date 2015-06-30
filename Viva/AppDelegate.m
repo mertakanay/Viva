f50187dfd562929e03c3ee4f674e20898ed8579b
mertakanay <mert.akanay@live.com>
2015-06-29 18:18:52 -0700

    first commit

--- Viva/AppDelegate.m
+++ Viva/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import GoogleMaps;
 
 @interface AppDelegate ()
 
@@ -16,6 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    [GMSServices provideAPIKey:@"AIzaSyDcSkk3PUi-DVbugmJdyiPtAp-oLwFYOlU"];
+
     // Override point for customization after application launch.
     return YES;
 }
--- Viva/MapViewController.m
+++ Viva/MapViewController.m
@@ -6,22 +6,66 @@
 //  Copyright (c) 2015 Viva. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MapViewController.h"
+#import <CoreLocation/CoreLocation.h>
+@import GoogleMaps;
 
-@interface ViewController ()
+//search bar with animation
+
+@interface MapViewController () <CLLocationManagerDelegate, GMSMapViewDelegate, UIGestureRecognizerDelegate>
+
+@property CLLocationManager *locationManager;
+@property GMSMapView *gMapView;
 
 @end
 
-@implementation ViewController
+@implementation MapViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+//    self.locationManager = [CLLocationManager new];
+//    self.locationManager.delegate = self;
+//    [self.locationManager requestWhenInUseAuthorization];
+
+    self.gMapView.delegate = self;
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude: 40.730610 longitude: -73.935242 zoom:10];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+//    CGFloat screenWidth = screenRect.size.width; //MAYBE I NEED TO CHANGE THIS FOR IPAD
+//    CGFloat screenHeight = screenRect.size.height;
+    self.gMapView = [GMSMapView mapWithFrame:screenRect camera:camera];
+    self.gMapView.mapType = kGMSTypeHybrid;
+    [self.view addSubview:self.gMapView];
+
+    [self markTheCity];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"Memory warning!");
+}
+
+-(void)createPolygonOnCity
+{
+    GMSMutablePath *rect = [GMSMutablePath path];
+    [rect addCoordinate:CLLocationCoordinate2DMake(37.36, -122.0)];
+    [rect addCoordinate:CLLocationCoordinate2DMake(37.45, -122.0)];
+    [rect addCoordinate:CLLocationCoordinate2DMake(37.45, -122.2)];
+    [rect addCoordinate:CLLocationCoordinate2DMake(37.36, -122.2)];
+
+    // Create the polygon, and assign it to the map.
+    GMSPolygon *polygon = [GMSPolygon polygonWithPath:rect];
+    polygon.fillColor = [UIColor colorWithRed:0.25 green:0 blue:0 alpha:0.05];
+    polygon.strokeColor = [UIColor blackColor];
+    polygon.strokeWidth = 2;
+    polygon.map = self.gMapView;
+}
+
+-(void)markTheCity
+{
+    UILongPressGestureRecognizer *selectCityPress = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(markTheCity)];
+    [self.view addGestureRecognizer:selectCityPress];
 }
 
 @end
